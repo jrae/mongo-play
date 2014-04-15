bdf29079df8bb4ae2b4d52bcdcbd404888351bba
pyruby <chris@pyruby.co.uk>
2014-04-15 18:01:35 +0100

    - Re-instate "_id" in place of "id"

--- loader.py
+++ loader.py
@@ -2,22 +2,22 @@ import urllib2, json
 import random
 
 assets = [
-{"id" : "vehicle1", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08617401123046874, 51.52487262675978]}},
-{"id" : "vehicle2", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.07836341857910156, 51.52528648801461]}},
-{"id" : "vehicle3", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08703231811523438, 51.52284331713511]}},
-{"id" : "vehicle4", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08934974670410155, 51.52599404854359]}},
-{"id" : "vehicle5", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08986473083496094, 51.52615424940099]}},
-{"id" : "vehicle6", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.07973670959472656, 51.52922465690234]}},
-{"id" : "vehicle7", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.076904296875     , 51.52492602842339]}},
-{"id" : "vehicle8", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08359909057617188, 51.52212235641814]}},
-{"id" : "vehicle9", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08990764617919922, 51.5212945726301 ]}},
-{"id" : "vehicle10", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08475780487060547, 51.52537994003543]}},
-{"id" : "vehicle11", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08065938949584961, 51.52513963445152]}}
+{"_id" : "vehicle1", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08617401123046874, 51.52487262675978]}},
+{"_id" : "vehicle2", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.07836341857910156, 51.52528648801461]}},
+{"_id" : "vehicle3", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08703231811523438, 51.52284331713511]}},
+{"_id" : "vehicle4", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08934974670410155, 51.52599404854359]}},
+{"_id" : "vehicle5", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08986473083496094, 51.52615424940099]}},
+{"_id" : "vehicle6", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.07973670959472656, 51.52922465690234]}},
+{"_id" : "vehicle7", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.076904296875     , 51.52492602842339]}},
+{"_id" : "vehicle8", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08359909057617188, 51.52212235641814]}},
+{"_id" : "vehicle9", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08990764617919922, 51.5212945726301 ]}},
+{"_id" : "vehicle10", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08475780487060547, 51.52537994003543]}},
+{"_id" : "vehicle11", "type" : "vehicle", "geometry" : { "type" : "point", "coordinates" : [-0.08065938949584961, 51.52513963445152]}}
 ]
 
 categories = [
-    {"id" : "vehicle", "icon" : {"name" : "vehicle", "colour" : "blue"}},
-    {"id" : "person", "icon" : {"name" : "person", "colour" : "red"}}
+    {"_id" : "vehicle", "icon" : {"name" : "vehicle", "colour" : "blue"}},
+    {"_id" : "person", "icon" : {"name" : "person", "colour" : "red"}}
 ]
 
 vehno = 20
